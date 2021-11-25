0f7b903a199d06d1a3d7d597f36d9a9d9cb3a841
Hayk <jomardyan@outlook.com>
2021-11-25 12:08:25 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace mintst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Write seconds");
+            int y = int.Parse(Console.ReadLine());
+            Console.WriteLine(calcmin(y));
+        }
+
+        static string calcmin(int seconds)
+        {
+            int Minutes = 0, Seconds = 0;
+            int i = seconds;
+            if (seconds < 60)
+            {
+                Seconds = seconds;
+            }
+            else if (seconds > 60)
+            
+            {
+                i = seconds % 60;
+                Minutes = (seconds - i) / 60;
+                Seconds = seconds;
+            }
+
+            return "Min: " + Minutes.ToString() + " Seconds: " + i;
+        }
+
+    }
+}
